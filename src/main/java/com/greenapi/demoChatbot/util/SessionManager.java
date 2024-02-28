0947794f957d7213d1b3761f0e915da6a50829d5
dany47788 <65661948+dany47788@users.noreply.github.com>
2024-02-28 12:25:58 -0300

    fix session manager

--- src/main/java/com/greenapi/demoChatbot/util/SessionManager.java
+++ src/main/java/com/greenapi/demoChatbot/util/SessionManager.java
@@ -10,12 +10,6 @@ public class SessionManager {
     public static boolean isSessionExpired(State currentState) {
         var timestamp = (LocalDateTime) currentState.getData().get("last_touch_timestamp");
 
-        boolean i = timestamp != null && Duration.between(timestamp, LocalDateTime.now()).toMinutes() >= 1;
-        System.out.println(timestamp);
-        System.out.println(i);
-        if (timestamp != null) {
-            System.out.println(Duration.between(timestamp, LocalDateTime.now()).toMinutes());
-        }
         if (timestamp != null && Duration.between(timestamp, LocalDateTime.now()).toMinutes() >= 1) {
             currentState.getData().put("last_touch_timestamp", LocalDateTime.now());
             return true;
